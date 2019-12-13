691626cec46993a867ce8a416129eb70623883bf
Dawid Pado <dawidpado97@gmail.com>
2019-12-13 23:06:25 +0100

    aggiunta about us

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -195,7 +195,7 @@
                 <div class="single-footer-widget">
                     <h6>About Us</h6>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+                        sito creato da Dawid Pado, Bryant Sarabia e Giacomo Sfratato
                     </p>
                 </div>
             </div>
@@ -253,11 +253,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            <p class="footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
         </div>
     </div>
 </footer>
