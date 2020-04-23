d6e65b7f6a825483f617a2db14abdb716bc245d8
Giacomo Sfratato <xbit18@hotmail.it>
2020-04-23 19:46:24 +0200

    added address and user migrations

---
+++ database/migrations/2020_04_23_165208_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('country');
+            $table->string('city');
+            $table->string('address');
+            $table->string('address_additional')->nullable();
+            $table->unsignedBigInteger('postal_code');
+            $table->enum('type', ['billing','delivery','both'])->default('both');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
