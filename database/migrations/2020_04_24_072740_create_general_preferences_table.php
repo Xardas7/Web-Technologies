7acccd391226eb32ed51ea1ee4a8d7d6bf584efd
Bryant Sarabia <bryantsarabia@gmail.com>
2020-04-24 10:00:09 +0200

    Avatars and General Preferences Migration

---
+++ database/migrations/2020_04_24_072740_create_general_preferences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGeneralPreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('general_preferences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->double('weight');
+            $table->double('length');
+            $table->double('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('general_preferences');
+    }
+}
