7fc66753533d375ea9ebaa9732c7e9ff535a5321
Bryant Sarabia <bryantsarabia@gmail.com>
2020-04-24 09:07:43 +0200

    first pull

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,18 @@
 <?php
 
-Route::get('/cart', function () {
-    return view('cart');
-});
-Route::get('/category', function () {
-    return view('category');
-});
-Route::get('/checkout', function () {
-    return view('checkout');
-});
-Route::get('/confermation', function () {
-    return view('confermation');
-});
-Route::get('/generic', function () {
-    return view('generic');
-});
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/single', function () {
-    return view('single');
-});
-Route::get('/tracking', function () {
-    return view('tracking');
-});
-Route::get('/elements', function () {
-    return view('elements');
-});
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('index');
-}); ?>
+    return view('welcome');
+});
