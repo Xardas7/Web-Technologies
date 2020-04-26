09475286e425954eab60de4c61116608fddc84b5
Giacomo Sfratato <xbit18@hotmail.it>
2020-04-26 02:26:42 +0200

    groups services

--- public/js/app.js
+++ public/js/app.js
@@ -49935,8 +49935,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\wamp64\www\Tecnologie del Web\Web-Technologies\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\wamp64\www\Tecnologie del Web\Web-Technologies\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Desktop\Progetti\WebTechnologies\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Desktop\Progetti\WebTechnologies\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
