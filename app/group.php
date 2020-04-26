862fecd7cf79bf404ce301b342fcf9656c6e6bef
Bryant Sarabia <bryantsarabia@gmail.com>
2020-04-26 14:50:10 +0200

    relazioni

--- app/group.php
+++ app/group.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class group extends Model
 {
-    //
+    public function services(){
+        return $this->belongsToMany('App\Service','group_has_services');
+    }
 }
--- app/service.php
+++ app/service.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class service extends Model
 {
-    //
+    public function groups(){
+        return $this->belongsToMany('App\Group','group_has_services');
+    }
 }
