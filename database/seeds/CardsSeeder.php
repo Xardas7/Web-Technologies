699ad7ad60c6ca30c55880a8ce24f1a784a9a159
Bryant Sarabia <bryantsarabia@gmail.com>
2020-04-27 13:21:13 +0200

    Address factory

---
+++ database/seeds/CardsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Card;
+use Illuminate\Database\Seeder;
+
+class CardsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Card::class,5)->create();
+    }
+}
