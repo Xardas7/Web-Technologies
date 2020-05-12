fca615691e2c37d63509e6ab1a44c5bcb97afca2
Xardas7 <taif12340@gmail.com>
2020-05-12 20:43:00 +0200

    most searched realoed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use App\Helpers\General\CollectionHelper;
+use App\mostSearchedProducts;
 use Illuminate\Http\Request;
 use App\Product;
 
@@ -52,6 +53,27 @@ class ProductController extends Controller
                 $product=$product1;
                 break;
             }
+        }
+
+        //update di most searched
+        //da qui controllo se e' stato gia cercato
+        $most_searched_products = MostSearchedProducts::all();
+        $most_searched_product = new MostSearchedProducts();
+        foreach ($most_searched_products as $most_searched_product1){
+            if($most_searched_product1->product_id == $product->id){
+                $most_searched_product=$most_searched_product1;
+            }
+        }
+        //se non e' stato mai cercato ne crea uno
+        if($most_searched_product->id == null){
+            $most_searched_product->product_id=$product->id;
+            $most_searched_product->count=1;
+            $most_searched_product->save();
+        }
+        //se è stato trovato aumenta count
+        else{
+            $most_searched_product->count=($most_searched_product->count+1);
+            $most_searched_product->save();
         }
         return view('single', compact('product'));
     }
