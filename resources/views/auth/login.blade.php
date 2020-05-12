2128a56b5b6eacc463412d475d7d178097f0e9b1
Xardas7 <taif12340@gmail.com>
2020-05-12 15:20:53 +0200

    few changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="login-form">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
