b4995f08890333ac75d1a3bd0420dd6f82a5bd1f
Giacomo Sfratato <xbit18@hotmail.it>
2020-05-14 17:29:29 +0200

    cart

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -12,25 +12,35 @@ class ShoppingCartController extends Controller
     public function store(Request $request){
         //Estraggo le info dell'utente loggato
         $user = Auth::user();
+
         //Estraggo le info del carrello dell'utente loggato
         $cart = ShoppingCart::where('user_id',$user->id)->first();
-        //Controllo se nel carrello dell'utente quel prodotto è già presente
-        $shoppingcarthasproducts = ShoppingCartsHaveProduct::where('shoppingcart_id',$cart->id)
-                                    ->where('product_id',$request->product_id)
-                                    ->first();
 
-        if($shoppingcarthasproducts){
-            //Se il prodotto è già presente, ne aumento solo la quantità
-            $shoppingcarthasproducts->quantity += $request->quantity;
-            $shoppingcarthasproducts->save();
-
-        } else {
-            //Altrimenti, lo inserisco
-            $shopping = new ShoppingCartsHaveProduct;
-            $shopping->shoppingcart_id = $cart->id;
-            $shopping->product_id = $request->product_id;
-            $shopping->quantity = $request->quantity;
-            $shopping->save();
+        //Controllo che il carrello esista, se esiste
+        if(!$cart) {
+            //Se non esiste
+            $cart = ShoppingCart::create([
+            'user_id'=>$user->id
+            ]);
         }
+
+            //Controllo se nel carrello dell'utente quel prodotto è già presente
+            $shoppingcarthasproducts = ShoppingCartsHaveProduct::where('shoppingcart_id', $cart->id)
+                ->where('product_id', $request->product_id)
+                ->first();
+
+            if ($shoppingcarthasproducts) {
+                //Se il prodotto è già presente, ne aumento solo la quantità
+                $shoppingcarthasproducts->quantity += $request->quantity;
+                $shoppingcarthasproducts->save();
+
+            } else {
+                //Altrimenti, lo inserisco
+                $shopping = new ShoppingCartsHaveProduct;
+                $shopping->shoppingcart_id = $cart->id;
+                $shopping->product_id = $request->product_id;
+                $shopping->quantity = $request->quantity;
+                $shopping->save();
+            }
     }
 }
--- app/ShoppingCart.php
+++ app/ShoppingCart.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShoppingCart extends Model
 {
+    protected $fillable=['user_id'];
+
     public function user(){
         return $this->belongsTo('App\User');
     }
