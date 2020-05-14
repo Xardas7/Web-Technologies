39c4f7eaed0167f52a1f0fb7960e6649f1796f76
Bryant Sarabia <bryantsarabia@gmail.com>
2020-05-14 14:52:51 +0200

    cors

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::options('/{path}', function(){ 
+    return '';
+})->where('path', '.*');
 
 Route::get('/', function () {
     return view('index');
