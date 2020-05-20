20a63a1a0bc41a20715577d69d2a48311b8318c2
Xardas7 <taif12340@gmail.com>
2020-05-20 14:58:35 +0200

    nav bar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -58,12 +58,17 @@
                             </a>
 
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">
+                                    My Orders
+                                </a>
+                                <a class="dropdown-item" href="#">
+                                    Settings
+                                </a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
