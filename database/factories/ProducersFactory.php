7f5ae4c5f7dd2e9f01f7efa761d9151e74ea4dab
Giacomo Sfratato <xbit18@hotmail.it>
2020-07-20 01:56:36 +0200

    fix

--- database/factories/ProducersFactory.php
+++ database/factories/ProducersFactory.php
@@ -8,8 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Producer::class, function (Faker $faker) {
     return [
         'name'=> $faker->unique()->company,
-        'logo' => $faker->imageUrl(60,60),
-        //  'logo'=>'https://i.picsum.photos/id/'.$faker->numberBetween(1,1000).'/250/250.jpg',
         'user_id' => $faker->unique()->numberBetween(1,50)
     ];
 });
