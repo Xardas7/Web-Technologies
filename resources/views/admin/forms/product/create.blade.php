cc3a66ce911dfa632f22609640aa6a2512ab09ff
Bryant Sarabia <bryantsarabia@gmail.com>
2020-07-19 18:28:34 +0200

    product upload

--- resources/views/admin/forms/product/create.blade.php
+++ resources/views/admin/forms/product/create.blade.php
@@ -11,11 +11,11 @@
 
     <div class="panel panel-default">
         <div class="panel-body">
-                <form method="POST" action="/admin/product/store">
+                <form method="POST" action="/admin/product/store" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
                         <label>Producer</label>
-                        <input type="text" class="form-control" placeholder="Producer" name="name">
+                        <input type="text" class="form-control" placeholder="Producer" name="producer">
                     </div>
                     <div class="form-group">
                         <label>Category Name</label>
@@ -27,51 +27,47 @@
                     </div>
                     <div class="form-group">
                         <label>Name</label>
-                        <input type="text"class="form-control" placeholder="Name" name="email">
+                        <input type="text"class="form-control" placeholder="Name" name="name">
                     </div>
                     <div class="form-group">
                         <label>Price</label>
-                        <input class="form-control" type="text" placeholder="Price" name="email">
+                        <input class="form-control" type="text" placeholder="Price" name="price">
                     </div>
                     <div class="form-group">
                         <label>Description</label>
-                        <input class="form-control" type="text" placeholder="Description" name="email">
+                        <input class="form-control" type="text" placeholder="Description" name="description">
                     </div>
                     <hr>
                     <h2>Details</h2>
                     <div class="form-group">
                         <label>Material</label>
-                        <input class="form-control" type="text"placeholder="Material" name="email">
+                        <input class="form-control" type="text"placeholder="Material" name="material">
                     </div>
                     <div class="form-group">
                         <label>Composition</label>
-                        <input class="form-control"type="text" placeholder="Composition" name="email">
+                        <input class="form-control"type="text" placeholder="Composition" name="composition">
                     </div>
                     <div class="form-group">
                         <label>Quantity</label>
-                        <input class="form-control"type="text" placeholder="Quantity" name="email">
+                        <input class="form-control"type="text" placeholder="Quantity" name="quantity">
                     </div>
                     <div class="form-group">
                         <label>Width</label>
-                        <input class="form-control"type="text" placeholder="Width" name="email">
+                        <input class="form-control"type="text" placeholder="Width" name="width">
                     </div>
                     <div class="form-group">
                         <label>Depth</label>
-                        <input class="form-control"type="text" placeholder="Depth" name="email">
+                        <input class="form-control"type="text" placeholder="Depth" name="depth">
                     </div>
                     <div class="form-group">
                         <label>Weight</label>
-                        <input class="form-control"type="text" placeholder="Weight" name="email">
+                        <input class="form-control"type="text" placeholder="Weight" name="weight">
                     </div>
                     <hr>
                     <h2>Images</h2>
                     <div class="form-group">
-                        <label>Images from file sistem</label>
-                        <input class="form-control" type="file" placeholder="Images" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label>Images from link</label>
-                        <input class="form-control" type="text" placeholder="Images" name="email">
+                        <label>Images from file system</label>
+                        <input class="form-control" type="file" placeholder="Images" name="images[]" required multiple>
                     </div>
                     @if ($errors->any())
                         <div class="alert alert-danger">
